061df3b753644ffe7b29152c79a20ff2f68a4f8c
trollcoin <trollcoin@trollcoin-ubuntu.(none)>
2014-01-09 21:59:11 -0500

    fix dependencies

--- trollcoin-build.sh
+++ trollcoin-build.sh
@@ -23,9 +23,11 @@ fi
 
 echo 'Installing dependencies...'
 apt-get -y update
-apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev
+apt-get -y install qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libminiupnpc-dev
 apt-get -y install libboost-all-dev
+apt-get -y remove libdb5.1-dev
 apt-get -y install libdb4.8-dev
+apt-get -y remove libdb5.1++-dev
 apt-get -y install libdb++-dev
 
 echo 'Building trollcoind'
